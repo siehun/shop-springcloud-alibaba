5ae8801f5ee6610026acd161b01ebda964ead9cc
siehun <126447514+siehun@users.noreply.github.com>
2025-02-22 16:56:30 +0800

    项目初始化

---
+++ shop-product/src/test/java/com/yue/shopproduct/ShopProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yue.shopproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
